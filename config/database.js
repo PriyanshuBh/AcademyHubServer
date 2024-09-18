739d68ac3ceeb825e674074821d604dfb707a2a2
PriyanshuBh <priyanshubh2003@gmail.com>
2024-09-18 21:16:45 +0530

    new project

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+export const connect = () => {
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("DB Connected Successfully"))
+    .catch((error) => {
+      console.log("DB Connection Failed")
+      console.error(error)
+      process.exit(1)
+    })
+}
